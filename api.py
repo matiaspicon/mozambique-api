27ee2d44a17d2743f064747bd025c0e23f4c19c7
MPICON <mpicon2@up.edu.ar>
2023-10-30 15:34:23 -0300

    add get method, add endpoiint inventory

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 db_path = "database.db"
 
-@app.route('/sales', methods=['GET','POST'])
-def guardar_y_recuperar_datos():
+@app.route('/inventory', methods=['GET','POST'])
+def inventory():
     if request.method == 'POST':
         data = request.get_json()
         if not data:
@@ -24,8 +24,48 @@ def guardar_y_recuperar_datos():
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
 
-        cursor.execute("INSERT INTO sales (user_id, barcode, invoice_number, supplier_id, n_units) VALUES (?, ?, ?, ?, ?)",
+        cursor.execute("INSERT INTO inventory (user_id, barcode, invoice_number, supplier_id, n_units) VALUES (?, ?, ?, ?, ?)",
                     (user_id, barcode, invoice_number, supplier_id, n_units))
+
+        conn.commit()
+        conn.close()
+
+        return jsonify({"message": "Data saved successfully"})
+    
+    elif request.method == 'GET':
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM inventory")
+        inventory = cursor.fetchall()
+
+        conn.close()
+
+        return jsonify({"inventory": inventory})
+
+
+@app.route('/sales', methods=['GET','POST'])
+def sales():
+    if request.method == 'POST':
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Invalid JSON format"}), 400
+
+        user_id = data.get('user_id')
+        barcode = data.get('barcode')
+        customer_name = data.get('customer_name')
+        customer_phone_number = data.get('customer_phone_number')
+        price = data.get('price')
+        n_units = data.get('n_units')
+
+        if not all([user_id, barcode, customer_name, customer_phone_number, price, n_units]):
+            return jsonify({"error": "Missing required fields"}), 400
+
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO sales (user_id, barcode, customer_name, customer_phone_number, price, n_units) VALUES (?, ?, ?, ?, ?, ?)",
+                    (user_id, barcode, customer_name, customer_phone_number, price, n_units))
 
         conn.commit()
         conn.close()
--- create-db.py
+++ create-db.py
@@ -6,12 +6,24 @@ conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS sales (
+    CREATE TABLE IF NOT EXISTS inventory (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER NOT NULL,
         barcode INTEGER NOT NULL,
         invoice_number TEXT NOT NULL,
         supplier_id INTEGER NOT NULL,
+        n_units INTEGER NOT NULL
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS sales (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        barcode INTEGER NOT NULL,
+        customer_name TEXT NOT NULL,
+        customer_phone_number TEXT NOT NULL,
+        price FLOAT NOT NULL,
         n_units INTEGER NOT NULL
     )
 ''')
